3ee7d8c0477ebef33fb2b5f60eb659ee7c7f198f
AnastasiiaKotsaba <anastasiakocaba49@gmail.com>
2025-12-04 18:12:30 +0200

    was added new endpoint - getProductById

--- src/Products/products.router.ts
+++ src/Products/products.router.ts
@@ -4,5 +4,7 @@ import { ProductController } from "./products.controller";
 const router = Router()
 
 router.get('/products', ProductController.getAllProducts)
+router.get('/product/:id', ProductController.getProductById)
+
 
 export default router
