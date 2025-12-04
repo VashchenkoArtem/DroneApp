25fc737e21e68e6de15c72fb180bfb5d6b078552
AnastasiiaKotsaba <anastasiakocaba49@gmail.com>
2025-12-04 18:15:17 +0200

    was added new endpoint - getProductById

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from "express"
 import type { Express } from "express"
 import productsRouter from './Products/products.router'
 
-const HOST:string = "127.0.0.1"
-const PORT:number = 8000
+const HOST: string = "127.0.0.1"
+const PORT: number = 8000
 const app: Express = express()
 
 app.use(express.json())
