88cf0e647ae5c58ce77c3951c55f0aeae5b71dad
Artem <artemvaschenko83@gmail.com>
2026-01-16 20:40:49 +0200

    Work with hooks at the home page

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,16 @@ import type { Express } from "express"
 import productsRouter from './Products/products.router'
 import categoriesRouter from './Categories/categories.router'
 import userRouter from "./User/user.router"
+import cors  from "cors"
 
 const HOST: string = "127.0.0.1"
 const PORT: number = 8000
 const app: Express = express()
 
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 app.use(express.json())
 app.use(productsRouter)
 app.use(categoriesRouter)
